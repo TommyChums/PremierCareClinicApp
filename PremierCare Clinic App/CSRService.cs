5c370c8c4c0149014aa22af16371ac31ad21fe24
Ricardo Felix <ricardofelix2@yahoo.com>
2019-04-03 23:15:02 -0400

    added comments and service + csr class

---
+++ PremierCare Clinic App/CSRService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierCare_Clinic_App
+{
+    public class CSRService {
+        csrDAO csrDAO = new csrDAO();
+
+        public bool CreateCSR(CSR csr) {
+	        return csrDAO.CreateCSR(csr);
+        }
+
+        public List<CSR> GetCSRS() {
+	        return csrDAO.GetCSRS();
+        }
+
+        public List<CSR> GetCSRS(string term) {
+	        return csrDAO.GetCSRS(term);
+        }
+
+        public bool UpdateCSR(CSR csr) {
+	        return csrDAO.UpdateCSR(csr);
+        }
+
+        public bool DeleteCSR(CSR csr) {
+	        return csrDAO.DeleteCSR(csr);
+        }
+    }
+}
---
+++ PremierCare Clinic App/ServiceService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierCare_Clinic_App
+{
+	//Service Implementation for the Service Class
+	//For Business Logic as well as DAO operations
+    public class ServiceService {
+		ServiceDAO serviceDAO = new ServiceDAO();
+
+		public bool CreateService(Service service) {
+			return serviceDAO.CreateService(service);
+		}
+
+		public List<Service> GetServices() {
+			return serviceDAO.GetServices();
+		}
+
+		public List<Service> GetServices(string term) {
+			return serviceDAO.GetServices(term);
+		}
+
+		public bool UpdateService(Service service) {
+			return serviceDAO.UpdateService(service);
+		}
+
+		public bool DeleteService(Service service) {
+			return serviceDAO.DeleteService(service);
+		}
+    }
+}
